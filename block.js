63c05cfd33e020d46bb3c7187690b48ee71f7d7a
Micah Wood <micah@wpscholar.com>
2018-04-09 23:57:15 -0400

    Updates to block: wrap in IIFE, fix class name bug, convert back to using RichText component, handle focusing properly.

--- block.js
+++ block.js
@@ -1,113 +1,138 @@
-var el = wp.element.createElement,
-    registerBlockType = wp.blocks.registerBlockType,
-    Editable = wp.blocks.Editable;
+(function (_, blocks, components, data, i18n, element) {
 
-registerBlockType('wpscholar/gutenbook', {
-    title: 'Book',
-    icon: 'book-alt',
-    category: 'layout',
-    attributes: {
-        title: {
-            type: 'text',
-            selector: 'div.wp-block-wpscholar-gutenbook__title',
-        },
-        description: {
-            type: 'html',
-            source: 'children',
-            selector: 'div.wp-block-wpscholar-gutenbook__description',
-        },
-        author: {
-            type: 'text',
-            selector: 'div.wp-block-wpscholar-gutenbook__author',
-        }
-    },
-    edit: function (props) {
-        return el(
-            'div',
-            {
-                className: props.className
+    var __ = i18n.__;
+    var el = element.createElement;
+    var RichText = blocks.RichText;
+
+    blocks.registerBlockType('wpscholar/gutenbook', {
+        title: __('Book', 'gutenbook'),
+        icon: 'book-alt',
+        category: 'layout',
+        attributes: {
+            title: {
+                type: 'text',
+                selector: 'div.wp-block-wpscholar-gutenbook__title',
             },
-            [
-                el(
-                    Editable,
-                    {
-                        key: 'title',
-                        tagName: 'div',
-                        className: props.className + '__title',
-                        onChange: function (title) {
-                            props.setAttributes({title: title});
-                        },
-                        placeholder: 'Adventures of Huckleberry Finn',
-                        value: props.attributes.title,
-                        isSelected: props.isSelected
-                    },
-                    props.attributes.title
-                ),
-                el(
-                    Editable,
-                    {
-                        key: 'description',
-                        tagName: 'div',
-                        className: props.className + '__description',
-                        multiline: 'p',
-                        onChange: function (description) {
-                            props.setAttributes({description: description});
-                        },
-                        placeholder: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                        value: props.attributes.description,
-                        isSelected: props.isSelected
-                    },
-                    props.attributes.description
-                ),
-                el(
-                    Editable,
-                    {
-                        key: 'author',
-                        tagName: 'div',
-                        className: props.className + '__author',
-                        onChange: function (author) {
-                            props.setAttributes({author: author});
-                        },
-                        placeholder: 'Mark Twain',
-                        value: props.attributes.author,
-                        isSelected: props.isSelected
-                    },
-                    props.attributes.author
-                ),
-            ]
-        );
-    },
-    save: function (props) {
-        props.className = 'wp-block-wpscholar-gutenbook';
-        return el(
-            'div',
-            {},
-            [
-                el(
+            description: {
+                type: 'html',
+                source: 'children',
+                selector: 'div.wp-block-wpscholar-gutenbook__description',
+            },
+            author: {
+                type: 'text',
+                selector: 'div.wp-block-wpscholar-gutenbook__author',
+            }
+        },
+        edit: components.withState({
+            focus: 'title',
+        })(function (props) {
+                return el(
                     'div',
                     {
-                        key: 'title',
-                        className: 'wp-block-wpscholar-gutenbook__title'
+                        className: props.className
                     },
-                    props.attributes.title
-                ),
-                el(
-                    'div',
-                    {
-                        key: 'description',
-                        className: 'wp-block-wpscholar-gutenbook__description'
-                    },
-                    props.attributes.description
-                ),
-                el(
-                    'div',
-                    {
-                        key: 'author',
-                        className: 'wp-block-wpscholar-gutenbook__author'
-                    },
-                    props.attributes.author
-                ),
-            ]
-        );
-    },
-});
+                    [
+                        el(
+                            RichText,
+                            {
+                                key: 'title',
+                                tagName: 'div',
+                                className: 'wp-block-wpscholar-gutenbook__title',
+                                keepPlaceholderOnFocus: true,
+                                onChange: function (title) {
+                                    props.setAttributes({title: title});
+                                },
+                                onFocus: function () {
+                                    props.setState({focus: 'title'});
+                                },
+                                placeholder: __('Adventures of Huckleberry Finn', 'gutenbook'),
+                                value: props.attributes.title,
+                                isSelected: props.isSelected && 'title' === props.focus
+                            },
+                            props.attributes.title
+                        ),
+                        el(
+                            RichText,
+                            {
+                                key: 'description',
+                                tagName: 'div',
+                                className: 'wp-block-wpscholar-gutenbook__description',
+                                keepPlaceholderOnFocus: true,
+                                multiline: 'p',
+                                onChange: function (description) {
+                                    props.setAttributes({description: description});
+                                },
+                                onFocus: function () {
+                                    props.setState({focus: 'description'});
+                                },
+                                placeholder: __('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'gutenbook'),
+                                value: props.attributes.description,
+                                isSelected: props.isSelected && 'description' === props.focus
+                            },
+                            props.attributes.description
+                        ),
+                        el(
+                            RichText,
+                            {
+                                key: 'author',
+                                tagName: 'div',
+                                className: 'wp-block-wpscholar-gutenbook__author',
+                                keepPlaceholderOnFocus: true,
+                                onChange: function (author) {
+                                    props.setAttributes({author: author});
+                                },
+                                onFocus: function () {
+                                    props.setState({focus: 'author'});
+                                },
+                                placeholder: __('Mark Twain', 'gutenbook'),
+                                value: props.attributes.author,
+                                isSelected: props.isSelected && 'author' === props.focus
+                            },
+                            props.attributes.author
+                        ),
+                    ]
+                );
+            }
+        ),
+        save: function (props) {
+            return el(
+                'div',
+                {},
+                [
+                    el(
+                        'div',
+                        {
+                            key: 'title',
+                            className: 'wp-block-wpscholar-gutenbook__title'
+                        },
+                        props.attributes.title
+                    ),
+                    el(
+                        'div',
+                        {
+                            key: 'description',
+                            className: 'wp-block-wpscholar-gutenbook__description'
+                        },
+                        props.attributes.description
+                    ),
+                    el(
+                        'div',
+                        {
+                            key: 'author',
+                            className: 'wp-block-wpscholar-gutenbook__author'
+                        },
+                        props.attributes.author
+                    ),
+                ]
+            );
+        },
+    });
+
+})(
+    window._,
+    window.wp.blocks,
+    window.wp.components,
+    window.wp.data,
+    window.wp.i18n,
+    window.wp.element
+);
